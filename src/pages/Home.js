7a436e84bd10209ae31624719380336250a60c55
Shaun Gedye <sgedye88@yahoo.com.au>
2020-07-16 18:14:40 +0800

    Made CSS X-Browser

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,32 +4,11 @@ function Home() {
   return (
     <section className="page-wrapper">
       <div>
-        <p>Home</p>
-        <p>Home</p>
-        <p>Home</p>
-      </div>
-      <div>
-        <p>Home</p>
-        <p>Home</p>
-        <p>Home</p>
-      </div>{" "}
-      <div>
         <p>
           Home Home Home Home Home Home Home Home Home Home Home Home Home Home
+          Home Home Home Home Home Home Home Home Home Home Home Home Home Home Home
           Home
         </p>
-        <p>
-          Home Home Home Home Home Home Home Home Home Home Home Home Home Home
-          Home
-        </p>
-        <p>
-          Home Home Home Home Home Home Home Home Home Home Home Home Home Home
-          Home
-        </p>
-      </div>{" "}
-      <div>
-        <p>Home</p>
-        <p>Home</p>
       </div>
     </section>
   )
