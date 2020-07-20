ed0a798d610610598ae091a085584b6d245d73b7
Shaun Gedye <sgedye88@yahoo.com.au>
2020-07-20 22:21:59 +0800

    Added content :rocket:

--- src/pages/About.js
+++ src/pages/About.js
@@ -4,8 +4,12 @@ function About() {
   return (
     <section className="page-wrapper">
       <div>
-        <p>About</p>
-        <p>About</p>
+        <p>
+          Midday way in 2013 until 2019, Jessica has been running a targeted intervention for struggling adolescents at Como Secondary College, in Western Australia. During this time, she has built a considerable reputation as an outstanding educator and an expert in explicit teaching and special needs education. In 2019 she was awarded a coveted and prestigious Commonwealth Bank / Schools Plus teaching award in recognition of her success in improving literacy and numeracy outcomes in at‐risk students. As a special education and instructional coach, Jessica has been instrumental in advancing the awareness, understanding and use of evidence‐based instruction to prevent low numeracy and literacy failures across the state and beyond. Being a sought‐after advisor to more than 40 other secondary schools, she continues to impart her knowledge by assisting schools with timetabling, screening processes, recruitment and teacher training.
+        </p>
+        <p>
+          July 2019, Jessica has taken on a new role as a literacy coach in the Literacy Guarantee Unit with the Department for Education in South Australia.
+        </p>
       </div>
     </section>
   )
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,6 +7,40 @@ function Home() {
     <Wrapper className="page-wrapper">
       <h2>Home</h2>
       <img src={HeroImage} alt="jessie" width="200px" />
+      <h2>Mission</h2>
+      <p>I work with schools to eradicate adolescent illiteracy so that no student leaves school unable to read well.</p>
+      <h2>Vision</h2>
+      <p>All schools have the moral obligation to ensure that all students, regardless of background, can read fluently.</p>
+      <h2>Values</h2>
+      <table>
+        <tbody>
+          <tr>
+            <td></td>
+            <td>
+              Commitment I work to ensure that literacy is the highest priority in education.
+            </td>
+          </tr>
+          <tr>
+            <td></td>
+            <td>
+              Integrity I am committed to honest, transparent relationships and accurate objective use of data.
+            </td>
+          </tr>
+          <tr>
+            <td></td>
+            <td>
+              Knowledge accumulated scientific knowledge is the basis of excellent practice.
+            </td>
+          </tr>
+          <tr>
+            <td></td>
+            <td>
+              Partnership I work collaboratively with partner schools, as part of a wider movement, to achieve my mission.
+            </td>
+          </tr>
+        </tbody>
+      </table>
+      <strong style={{'fontSize': '1.2em', 'color': 'blue'}}>JESS: Write a paragraph about my life philosophy/moral purpose.</strong>
     </Wrapper>
   )
 }
