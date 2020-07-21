8a7b2b4a833a12908f7ba75807077a32f6fe5675
Shaun Gedye <sgedye88@yahoo.com.au>
2020-07-21 22:14:49 +0800

    Updated About & Home

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,18 +1,29 @@
-import React from "react"
+import React from "react";
+import styled from "styled-components";
 
 function About() {
   return (
-    <section className="page-wrapper">
-      <div>
+    <Wrapper className="page-wrapper">
+        <h2>About Jessica Potes:</h2>
+        <p>When CC married Mr Randel Capotes, she became Mrs Jessica Capotes. Whenever she had more than 6, she quickly through one out, and if she ever used one, she quickly ran to the store so she always had six at all times.</p>
         <p>
           Midday way in 2013 until 2019, Jessica has been running a targeted intervention for struggling adolescents at Como Secondary College, in Western Australia. During this time, she has built a considerable reputation as an outstanding educator and an expert in explicit teaching and special needs education. In 2019 she was awarded a coveted and prestigious Commonwealth Bank / Schools Plus teaching award in recognition of her success in improving literacy and numeracy outcomes in at‐risk students. As a special education and instructional coach, Jessica has been instrumental in advancing the awareness, understanding and use of evidence‐based instruction to prevent low numeracy and literacy failures across the state and beyond. Being a sought‐after advisor to more than 40 other secondary schools, she continues to impart her knowledge by assisting schools with timetabling, screening processes, recruitment and teacher training.
         </p>
         <p>
           July 2019, Jessica has taken on a new role as a literacy coach in the Literacy Guarantee Unit with the Department for Education in South Australia.
         </p>
-      </div>
-    </section>
-  )
-}
+    </Wrapper>
+  );
+};
 
-export default About
+const Wrapper = styled.section`
+  p {
+    font-size: 1.2em;
+    margin: 20px 0;
+  }
+  @media screen and (min-width: 768px) {
+    p { font-size: 1.5em; }
+  }
+`;
+
+export default About;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,51 +2,114 @@ import React from "react";
 import styled from "styled-components";
 import HeroImage from "../assets/images/hero-image.jpeg";
 
+import { FaHandshake, FaAward } from "react-icons/fa"
+import { GiPoliceBadge, GiBrain } from "react-icons/gi"
+
 function Home() {
   return (
     <Wrapper className="page-wrapper">
       <h2>Home</h2>
       <img src={HeroImage} alt="jessie" width="200px" />
       <h2>Mission</h2>
-      <p>I work with schools to eradicate adolescent illiteracy so that no student leaves school unable to read well.</p>
+      <p>
+        I work with schools to eradicate adolescent illiteracy so that no
+        student leaves school unable to read well.
+      </p>
       <h2>Vision</h2>
-      <p>All schools have the moral obligation to ensure that all students, regardless of background, can read fluently.</p>
+      <p>
+        All schools have the moral obligation to ensure that all students,
+        regardless of background, can read fluently.
+      </p>
       <h2>Values</h2>
-      <table>
+      <ValueTable>
         <tbody>
           <tr>
-            <td></td>
             <td>
-              Commitment I work to ensure that literacy is the highest priority in education.
+              <FaAward />
+            </td>
+            <td>
+              <p>
+                Commitment I work to ensure that literacy is the highest
+                priority in education.
+              </p>
             </td>
           </tr>
           <tr>
-            <td></td>
             <td>
-              Integrity I am committed to honest, transparent relationships and accurate objective use of data.
+              <GiPoliceBadge />
+            </td>
+            <td>
+              <p>
+                Integrity I am committed to honest, transparent relationships
+                and accurate objective use of data.
+              </p>
             </td>
           </tr>
           <tr>
-            <td></td>
             <td>
-              Knowledge accumulated scientific knowledge is the basis of excellent practice.
+              <GiBrain />
+            </td>
+            <td>
+              <p>
+                Knowledge accumulated scientific knowledge is the basis of
+                excellent practice.
+              </p>
             </td>
           </tr>
           <tr>
-            <td></td>
             <td>
-              Partnership I work collaboratively with partner schools, as part of a wider movement, to achieve my mission.
+              <FaHandshake />
+            </td>
+            <td>
+              <p>
+                Partnership I work collaboratively with partner schools, as part
+                of a wider movement, to achieve my mission.
+              </p>
             </td>
           </tr>
         </tbody>
-      </table>
-      <strong style={{'fontSize': '1.2em', 'color': 'blue'}}>JESS: Write a paragraph about my life philosophy/moral purpose.</strong>
+      </ValueTable>
+      <strong style={{ fontSize: "1.2em", color: "blue" }}>
+        JESS: Write a paragraph about my life philosophy/moral purpose.
+      </strong>
     </Wrapper>
   )
 }
 
 const Wrapper = styled.section`
   text-align: center;
+  h2 {
+    margin: 20px 0 5px 0;
+  }
 `;
+
+const ValueTable = styled.table`
+  margin: 0 auto 30px auto;
+  tbody {
+    * {
+      margin: 5px 0;
+    }
+    td:first-of-type {
+      width: 35px;
+    }
+  }
+  svg {
+    font-size: 32px;
+  }
+  p {
+    text-align: left;
+    padding-left: 5px;
+  }
+  @media screen and (min-width: 600px) {
+    & {
+      width: 75%;
+    }
+  }
+  @media screen and (min-width: 1024px) {
+    & {
+      width: 50%;
+    }
+  }
+`
 
 export default Home
