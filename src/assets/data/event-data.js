50bc08583a50a80ba7486e026ea6dc0120835315
Shaun Gedye <sgedye88@yahoo.com.au>
2020-07-28 13:45:06 +0800

    :construction_worker: Adding New Events to Events Page :hammer:

--- src/assets/data/event-data.js
+++ src/assets/data/event-data.js
@@ -1,15 +1,61 @@
 import React from "react";
+import leadersConf from "../images/events/leadersConf.png";
+import literacy2020 from "../images/events/literacy2020.png";
+import literacy2021 from "../images/events/literacy2021.png";
 import perthSSP from "../images/events/perthSSP.jpeg";
 import researchEd from "../images/events/researchEd.png";
+import researchEd2019 from "../images/events/researchEd2019.png";
+import studyTour2019 from "../images/events/studyTour2019.png";
 import webinars2020 from "../images/events/webinars2020.jpg";
 import uniteSPLD from "../images/events/uniteSPLD.png";
 
+import researchEdProgram from "../doc/ResearchEd_2019.pdf";
+
 export default [
+  // {
+  //   id: "event-",
+  //   name: "",
+  //   date: "",
+  //   location: "",
+  //   image: uniteSPLD,
+  //   description: (
+  //     <p>
+
+  //     </p>
+  //   ),
+  //   links: [
+  //     {
+  //       id: "",
+  //       title: "",
+  //       src: ""
+  //     },
+  //   ],
+  // },
+  {
+    id: "event-literacy-2021",
+    name: "Language, Literacy & Learning Conference",
+    date: "March 18-20, 2021",
+    location: "Perth, Western Australia",
+    image: literacy2021,
+    description: (
+      <p>
+        Preparations are well and truly underway for the third biennial DSF Language, Literacy and Learning Conference.<br /><br />
+        The Dyslexia-SPELD Foundation WA (DSF) provides services designed to benefit all children and adults struggling with literacy and/or numeracy acquisition.
+      </p>
+    ),
+    links: [
+      {
+        id: "event-literacy-2021-link-0",
+        title: "Join the mailing list",
+        src: "https://t.co/vhimog1hHA?amp=1"
+      },
+    ],
+  },
   {
     id: "event-0",
-    name: "Perth ResearchED",
-    date: "October 17, 2020",
-    location: "Western Australia",
+    name: "ResearchED 2020",
+    date: "October 17, 2020 (Full Day)",
+    location: "Perth, Western Australia",
     image: researchEd,
     description: (
       <p>
@@ -40,17 +86,15 @@ export default [
   },
   {
     id: "event-1",
-    name: "Perth Sharing Successful Practice",
-    date: "October 9, 2020",
-    location: "Western Australia",
+    name: "Sharing Successful Practice",
+    date: "October 9, 2020 (Full Day)",
+    location: "Perth, Western Australia",
     image: perthSSP,
     description: (
       <p>
-        Perth Sharing Successful Practice brings together academics,
-        administrators and classroom teachers who demonstrate evidence-based
-        practice. Keynote Speakers: Emeritus Professor Bill Louden, Professor
-        Stephen Dinham, Dr Catherine Scott, Dr Jennifer Buckingham, Associate
-        Professor Lorraine Hammond and Ollie Lovell.
+        Perth Sharing Successful Practice brings together academics, administrators and classroom teachers who demonstrate evidence-based practice.<br /><br />
+        Keynote Speakers: Emeritus Professor Bill Louden, Professor Stephen Dinham, Dr Catherine Scott, Dr Jennifer Buckingham, Associate Professor Lorraine Hammond and Ollie Lovell.<br /><br />
+        Jessica is part of the steering committee and will be presenting two workshops.
       </p>
     ),
     links: [
@@ -142,4 +186,128 @@ export default [
       },
     ],
   },
+  {
+    id: "event-literacy-2020",
+    name: "The Literacy Guarantee Statewide Conference",
+    date: "January 16th, 2020 (Full Day)",
+    location: "Adelaide, South Australia",
+    image: literacy2020,
+    description: (
+      <p>
+        Educational leaders and teachers from the Department for Education, Catholic Education SA and the Association of Independent Schools of SA are invited to attend the next Literacy Guarantee Conference which will feature a range of leading experts in literacy to support leaders and teachers more effectively meet the needs of all students.<br /><br />
+        Keynote address: Dr Tanya Serry. Senior Lecturer, Speech Pathology, La Trobe University. Jessica presented two 90-minute workshops on "The key components of an effective literacy program to support struggling adolescent readers" and "Using classroom videos to deconstruct and analyse teacher practice"
+      </p>
+    ),
+    links: [
+      {
+        id: "event-literacy-2020-link-0",
+        title: "Conference Guide",
+        src: "https://www.education.sa.gov.au/sites/default/files/literacy-guarantee-conference-guide.pdf"
+      },
+    ],
+  },
+  {
+    id: "event-leaders-2019",
+    name: "Leaders of today for leaders of tomorrow Conference 2019",
+    date: "October 19-20, 2019",
+    location: "Sydney, New South Wales",
+    image: leadersConf,
+    description: (
+      <p>
+        33 of the country’s accomplished teachers and school leaders converged in Sydney to discuss the best ways to make their students and schools future ready. Each Fellow is a recipient of a prestigious <a href="https://teachingawards.com.au/" target="_blank" rel="noreferrer noopener">Commonwealth Bank Teaching Award</a>, and together, provide a breadth and depth of teaching and school leadership experience and expertise in a range of Australian school settings.  Many of these schools are in communities where complexities and challenges of low socioeconomic disadvantage exist.<br /><br />
+        The Forum provided Fellows with the opportunity to connect, to be challenged by new ideas, and to explore ways to share their learnings with the broader teaching profession. 
+      </p>
+    ),
+    links: [
+      {
+        id: "event-leaders-2019-link-0",
+        title: "View Blog on Forum",
+        src: "https://www.schoolsplus.org.au/blog/leaders-today-leaders-tomorrow-ideas-from-our-teaching-fellows/"
+      },
+    ],
+  },
+  {
+    id: "event-brighton-2019",
+    name: "researchED Conference",
+    date: "August 24, 2019",
+    location: "Brighton, Victoria",
+    image: researchEd2019,
+    description: (
+      <p>
+        ResearchED returns to Australia with another day of evidence, discussion, challenge, networking and provocation. Education is in a period of huge upheaval, as the evidence revolution continues. Old ways of working are now being scrutinised, criticised, updated and improved, and we’ve started to see dynamic and exciting new ways of teacher-led involvement of rebooting the classroom.<br /><br />
+        The speaker schedule and tickets can be bought on the <a href="https://researched.org.uk/event/researched-melbourne-2019/" target="_blank" rel="noopener noreferrer">researchED event page</a>. 
+        Jessica presented a workshop titled "Remediating adolescent literacy using correcting Reading and Spelling Mastery (Grades 7 to 10)".
+      </p>
+    ),
+    links: [
+      {
+        id: "event-brighton-2019-link-0",
+        title: "View Program",
+        src: researchEdProgram
+      },
+    ],
+  },
+  {
+    id: "event-study-tour-2019",
+    name: "Teaching Fellow - Study Tour",
+    date: "July, 2019",
+    location: "Singapore, Singapore",
+    image: studyTour2019,
+    description: (
+      <p>
+        Alongside 11 other leading educators from throughout Australia, I was fortunate to embark on a week-long study tour of Singapore in July. The tour was part of a 12-month Fellowship awarded to me through the annual Commonwealth Bank Teaching Awards, an initiative run in partnership with education charity Schools Plus.<br /><br />
+        During the tour we visited a number of leading education institutes and schools, including the National Institute of Education, Ngee Ann Secondary School, the Academy of Singapore Teachers, Nan Chiau Primary School, the Academy of Principals Singapore, and Crest Secondary School.<br /><br />
+        The Commonwealth Bank Teaching Awards are run in partnership with Schools Plus, a not-for-profit organisation, which was established in 2013 to increase philanthropy for schools that most need support. Launched in 2016, the awards are run annually to recognise and reward 12 teachers across Australia with a $45,000 Teaching Fellowship including a Study Exchange Tour to Singapore. The majority of the recipients are teachers or school leaders working in schools in disadvantaged communities.
+      </p>
+    ),
+    links: [
+      {
+        id: "event-study-tour-2019-link-0",
+        title: "School Visit",
+        src: "https://visitsatsst.wordpress.com/2019/07/11/visit-by-australian-schools-plus-11-july-2019/"
+      },
+    ],
+  },
+
+
+
+  // {
+  //   id: "event-",
+  //   name: "",
+  //   date: "",
+  //   location: "",
+  //   image: uniteSPLD,
+  //   description: (
+  //     <p>
+        
+  //     </p>
+  //   ),
+  //   links: [
+  //     {
+  //       id: "",
+  //       title: "",
+  //       src: ""
+  //     },
+  //   ],
+  // },
+  // },
+  // {
+  //   id: "event-",
+  //   name: "",
+  //   date: "",
+  //   location: "",
+  //   image: uniteSPLD,
+  //   description: (
+  //     <p>
+        
+  //     </p>
+  //   ),
+  //   links: [
+  //     {
+  //       id: "",
+  //       title: "",
+  //       src: ""
+  //     },
+  //   ],
+  // },
 ]
