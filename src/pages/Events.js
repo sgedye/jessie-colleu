8fc258d99f8b2d4503d29cbeebfa14beb0c597a4
Shaun Gedye <sgedye88@yahoo.com.au>
2020-07-29 16:24:05 +0800

    :rocket: Added GSAP Animations :sparkles:

--- src/pages/Events.js
+++ src/pages/Events.js
@@ -1,18 +1,48 @@
-import React from "react";
-
+import React, { useEffect, useRef } from "react";
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 import eventData from '../assets/data/event-data';
 import styles from "../assets/scss/events.module.scss";
 
 import { FaMapMarkerAlt, FaExternalLinkAlt } from "react-icons/fa"
+gsap.registerPlugin(ScrollTrigger);
 
 const Events = () => {
+
+  const eventListRef = useRef([]);
+  eventListRef.current = [];
+
+  useEffect(() => {
+    eventListRef.current.forEach((event, index) => {
+      gsap.fromTo(event, {
+        autoAlpha: 0
+      }, {
+        autoAlpha: 1,
+        duration: 1.5,
+        ease: 'none',
+        scrollTrigger: {
+          id: `event-${index}`,
+          trigger: event,
+          start: `top bottom-=200`,
+          toggleActions: `play none none none`,
+        }
+      });
+    })
+  }, [])
+
+  const addToRef = event => {
+    if (event && !eventListRef.current.includes(event)) {
+      eventListRef.current.push(event);
+    }
+  }
+
   return (
     <section className="page-wrapper">
       <h1 className="section-title">What's on:</h1>
       <br />
       {
         eventData.map(({ id, name, date, location, image, description, links }) => (
-          <div key={id} className={styles.eventCard}>
+          <div key={id} ref={addToRef} className={styles.eventCard}>
             <div className={styles.header}>
               <p className={styles.name}>{name}</p>
               <p className={styles.date}>{date}</p>
@@ -39,7 +69,7 @@ const Events = () => {
         ))
       }
     </section>
-  )
-}
+  );
+};
 
-export default Events
+export default Events;
